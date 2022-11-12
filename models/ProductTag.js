11757457fdd02b7c4577cc597aef5ca40e731315
BradDWagner <110359842+BradDWagner@users.noreply.github.com>
2022-11-11 20:35:27 -0800

    created Category routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,7 +8,7 @@ ProductTag.init(
   {
     // define columns
     id:{
-      type: DataTyes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
